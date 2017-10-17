6747828853f90b8a8a604c39222f7b4ee2d33a4d
blueslee <bluezhin@gmail.com>
2017-10-17 18:00:31 +0800

    first commit

---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Scroller : MonoBehaviour
+{
+    private float count = 3;
+    private float speed = 100;
+    private float wrapPoint = 400;
+    private float position;
+
+    void Start()
+    {
+        position = Vector3.Dot(this.transform.parent.transform.position, this.transform.parent.transform.forward);
+    }
+
+    void Update()
+    {
+        position += Time.deltaTime * speed;
+
+        if (position > wrapPoint) position -= wrapPoint * count;
+
+        this.transform.parent.transform.position = this.transform.parent.transform.forward * position;
+    }
+}
